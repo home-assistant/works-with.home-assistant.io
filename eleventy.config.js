3872488e6a615fcee62ac39865a6374821f2d3ec
Darren Griffin <darren.griffin@live.co.uk>
2025-04-02 11:44:38 +0100

    Add cache busting

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,6 +14,8 @@ export default async function (eleventyConfig) {
 			"src/svg": "svg",
 		});
 
+	eleventyConfig.addGlobalData("CACHE_KEY", btoa("" + new Date().valueOf()).replaceAll("=", ""));
+
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
